ac1d390b45069c3769829b3ee5a7f653c71c42a5
Leandro <lterrado@gmail.com>
2020-04-27 18:17:14 -0300

    add firebase sign in

--- src/component/SignIn/SignIn.jsx
+++ src/component/SignIn/SignIn.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react"
 import FormInput from "../formInput/formInput"
 import CustomButton from "../CustomButton/CustomButton"
 
-import { signInWithGoogle } from "../../firebase/firebase.utils"
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils"
 
 import "./SignIn.scss"
 
@@ -17,13 +17,20 @@ class SignIn extends Component {
     }
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault()
 
-    this.setState({
-      email: "",
-      password: "",
-    })
+    const { email, password } = this.state
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password)
+      this.setState({
+        email: "",
+        password: "",
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   handleChange = (event) => {
