7175804d538327d7eb265ff0f76a81ba9ece422d
Leandro <lterrado@gmail.com>
2020-05-04 18:36:11 -0300

    Added to dropdown cart menu the cartitem component with reducer and actions

--- src/component/CartDropDown/CartDropDown.jsx
+++ src/component/CartDropDown/CartDropDown.jsx
@@ -1,14 +1,25 @@
 import React from "react"
+import { connect } from "react-redux"
+
+import CartItem from "../CartItem/CartItem"
 
 import CustomButton from "../CustomButton/CustomButton"
 
 import "./CartDropDown.scss"
 
-const CartDropDown = () => (
+const CartDropDown = ({ cartItems }) => (
   <div className='cart-dropdown'>
-    <div className='cart-items'></div>
+    <div className='cart-items'>
+      {cartItems.map((cartItem) => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>Go to Checkout</CustomButton>
   </div>
 )
 
-export default CartDropDown
+const mapStateToProp = ({ cart: { cartItems } }) => ({
+  cartItems,
+})
+
+export default connect(mapStateToProp)(CartDropDown)
