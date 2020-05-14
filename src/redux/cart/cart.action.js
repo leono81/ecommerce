56e5575dbf74eb0d37e6f37b5ff5e8950ea419cd
Leandro <lterrado@gmail.com>
2020-05-14 14:57:47 -0300

    It was add the increase and decrease quantity on the checkout page

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -5,6 +5,16 @@ export const addItem = (item) => ({
   payload: item,
 })
 
+export const removeItem = (item) => ({
+  type: cartActionType.REMOVE_ITEM,
+  payload: item,
+})
+
 export const toggleCartHidden = () => ({
   type: cartActionType.TOGGLE_CART_HIDDEN,
+})
+
+export const clearItemFromCart = (item) => ({
+  type: cartActionType.CLEAR_ITEM_FROM_CART,
+  payload: item,
 })
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -15,3 +15,22 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   //if it doesn't, means that product is new ,so we will entry 1 to quantity product
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  //Check if item Exist before remove or decrease quantity
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  )
+
+  //If the quantity of the item to decrease is equal to 1 before decrease it, then remove the item
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id)
+  }
+
+  //Decrease the quantity of the item
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  )
+}
