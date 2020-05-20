1a7a19599a0f8d7a779d564d0bb19ba56a402965
Leandro <lterrado@gmail.com>
2020-05-19 22:30:56 -0300

    move directory and Shop Data to our  redux  folder and update our selectors to this.

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,26 +1,17 @@
-import React, { Component } from "react"
-import ShopData from "./ShopData"
-import CollectionPreview from "../../component/CollectionPreview/CollectionPreview"
+import React from "react"
+import { Route } from "react-router-dom"
 
-class ShopPage extends Component {
-  constructor(props) {
-    super(props)
+import CollectionsOverview from "../../component/CollectionsOverview/CollectionsOverview"
+import CollectionPage from "../Collection/Collection"
 
-    this.state = {
-      collections: ShopData,
-    }
-  }
-
-  render() {
-    const { collections } = this.state
-    return (
-      <div>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    )
-  }
+const ShopPage = ({ match }) => {
+  console.log(match)
+  return (
+    <div className='shop-page'>
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+  )
 }
 
 export default ShopPage
